6b4eada9e2594c71bfc5456724ffaef9e692a43f
Gênesis Aquino <genesiscaquino.2016@gmail.com>
2020-06-23 00:25:33 -0300

    Interface principal

--- app/src/main/java/com/example/organize_me/activity/PrincipalActivity.java
+++ app/src/main/java/com/example/organize_me/activity/PrincipalActivity.java
@@ -8,22 +8,30 @@ import com.example.organize_me.config.ConfiguracaoFirebase;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.TextView;
 
 public class PrincipalActivity extends AppCompatActivity {
 
-
+    private MaterialCalendarView calendarView;
+    private TextView textoSaudacao, textoSaldo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
 
-
+        calendarView = findViewById(R.id.calendarView);
+        textoSaudacao = findViewById(R.id.textSaudacao);
+        textoSaldo = findViewById(R.id.textSaldo);
+        configuraCalendarView();
 
 
         /*
@@ -39,6 +47,19 @@ public class PrincipalActivity extends AppCompatActivity {
          */
     }
 
+    private void configuraCalendarView() {
+
+        CharSequence meses[] = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+        calendarView.setTitleMonths( meses );
+
+        calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+
+            }
+        });
+    }
+
     public void adicionarDespesa(View view){
         startActivity(new Intent(PrincipalActivity.this, DespesasActivity.class));
     }
@@ -46,5 +67,7 @@ public class PrincipalActivity extends AppCompatActivity {
     public void adicionarReceita(View view){
         startActivity(new Intent(PrincipalActivity.this, ReceitasActivity.class));
     }
+
+
 
 }
