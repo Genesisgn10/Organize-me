2443ffe4140bf6049234e6f6bd8a77b2d1898bd1
Gênesis Aquino <genesiscaquino.2016@gmail.com>
2020-06-26 18:26:46 -0300

    listando movimentacoes

--- app/src/main/java/com/example/organize_me/activity/PrincipalActivity.java
+++ app/src/main/java/com/example/organize_me/activity/PrincipalActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.organize_me.R;
+import com.example.organize_me.adapter.AdapterMovimentacao;
 import com.example.organize_me.config.ConfiguracaoFirebase;
 import com.example.organize_me.helper.Base64Custom;
+import com.example.organize_me.model.Movimentacao;
 import com.example.organize_me.model.Usuario;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -19,6 +21,8 @@ import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.Menu;
 import android.view.MenuItem;
@@ -26,6 +30,8 @@ import android.view.View;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PrincipalActivity extends AppCompatActivity {
 
@@ -36,9 +42,17 @@ public class PrincipalActivity extends AppCompatActivity {
     private Double resumoTotal = 0.0;
 
     private FirebaseAuth autenticacao =  ConfiguracaoFirebase.getFirebaseAutenticacao();
-    private DatabaseReference fiDatabaseReference = ConfiguracaoFirebase.getFirebaseDatabase();
-    private  DatabaseReference usuarioRef;
+    private DatabaseReference firabaseRef = ConfiguracaoFirebase.getFirebaseDatabase();
+    private DatabaseReference usuarioRef;
     private ValueEventListener valueEventListenerUser;
+    private ValueEventListener valueEventListenerMovimentacao;
+
+    private RecyclerView recyclerView;
+    private AdapterMovimentacao adapterMovimentacao;
+    private List<Movimentacao> movimentacaos = new ArrayList<>();
+    private DatabaseReference movimentacaoRef;
+    private String mesAnoSelecionado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,12 +62,20 @@ public class PrincipalActivity extends AppCompatActivity {
         //toolbar.setTitle("Organizze-me");
         setSupportActionBar(toolbar);
 
-
         calendarView = findViewById(R.id.calendarView);
+        recyclerView = findViewById(R.id.recyclerMovimentos);
         textoSaudacao = findViewById(R.id.textSaudacao);
         textoSaldo = findViewById(R.id.textSaldo);
         configuraCalendarView();
 
+        //Configurando adapter
+        adapterMovimentacao = new AdapterMovimentacao(movimentacaos, this);
+
+        //Configurando RecyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager( layoutManager );
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter( adapterMovimentacao );
 
     }
 
@@ -62,10 +84,18 @@ public class PrincipalActivity extends AppCompatActivity {
         CharSequence meses[] = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
         calendarView.setTitleMonths( meses );
 
+        CalendarDay dataAtual = calendarView.getCurrentDate();
+        String mesSelecionado =  String.format( "%02d", (dataAtual.getMonth() + 1) );
+        mesAnoSelecionado = String.valueOf( mesSelecionado + "" + dataAtual.getYear() );
+
         calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
             @Override
             public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+                String mesSelecionado = String.format( "%02d", (date.getMonth() + 1) );
+                mesAnoSelecionado = String.valueOf( mesSelecionado + "" + date.getYear() );
 
+                movimentacaoRef.removeEventListener( valueEventListenerMovimentacao );
+                recuperarMovimentacoes();
             }
         });
     }
@@ -96,10 +126,45 @@ public class PrincipalActivity extends AppCompatActivity {
         startActivity(new Intent(PrincipalActivity.this, ReceitasActivity.class));
     }
 
+    private void recuperarMovimentacoes(){
+
+        String emailUsuario = autenticacao.getCurrentUser().getEmail();
+        String idUsuario = Base64Custom.codificarBase64( emailUsuario );
+        movimentacaoRef = firabaseRef.child("usuarios")
+                .child(idUsuario);
+
+        movimentacaoRef.child("movimentacao")
+                        .child( idUsuario )
+                        .child( mesAnoSelecionado );
+
+        valueEventListenerMovimentacao = movimentacaoRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                movimentacaos.clear();
+
+                for( DataSnapshot dados: dataSnapshot.getChildren() ){
+
+                    Movimentacao movimentacao = dados.getValue( Movimentacao.class );
+                    movimentacaos.add( movimentacao );
+
+                }
+
+                adapterMovimentacao.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
     private void recuperarResumo(){
         String emailUsuario = autenticacao.getCurrentUser().getEmail();
         String idUsuario = Base64Custom.codificarBase64( emailUsuario );
-        usuarioRef = fiDatabaseReference.child("usuarios")
+        usuarioRef = firabaseRef.child("usuarios")
                 .child(idUsuario);
 
         valueEventListenerUser = usuarioRef.addValueEventListener(new ValueEventListener() {
@@ -127,14 +192,16 @@ public class PrincipalActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
-        usuarioRef.removeEventListener( valueEventListenerUser );
-    }
-
-    @Override
     protected void onStart() {
         super.onStart();
         recuperarResumo();
+        recuperarMovimentacoes();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        usuarioRef.removeEventListener( valueEventListenerUser );
+        movimentacaoRef.removeEventListener( valueEventListenerMovimentacao );
     }
 }
